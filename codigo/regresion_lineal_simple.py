889cb601b1a8c9e218332844e82cc100023e6a92
Dante Grassi <dantegrassicai@gmail.com>
2026-03-20 10:56:34 -0300

    import fix

--- codigo/regresion_lineal_simple.py
+++ codigo/regresion_lineal_simple.py
@@ -57,3 +57,4 @@ for col in cols:
 
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
